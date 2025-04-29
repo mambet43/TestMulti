c8a9115c6f4fa5877852b073236521a7abada69f
Rustam M <mambet43@outlook.com>
2025-04-29 23:10:49 +0300

    29.04

--- ViewModels/MainPage.cs
+++ ViewModels/MainPage.cs
@@ -1,8 +1,66 @@
+using LiveChartsCore.Defaults;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView.Extensions;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using SkiaSharp;
+using LiveChartsCore.SkiaSharpView.Painting;
+using CommunityToolkit.Mvvm.Input;
+using LiveChartsCore.Drawing;
+using LiveChartsCore.Kernel.Events;
+using System.Diagnostics;
+using LiveChartsCore.Kernel;
+
 namespace TestMulti.ViewModels;
 
-public class MainPage : ContentView
+public partial class MainPage : ContentView
 {
-	public MainPage()
+    private readonly HashSet<ChartPoint> _activePoints = [];
+    [RelayCommand]
+    public void OnPressed(PointerCommandArgs args)
+    {
+        var foundPoints = args.Chart.GetPointsAt(args.PointerPosition);
+
+        // Проверяем, что в коллекции достаточно элементов
+        if (foundPoints.Count() >= 2)
+        {
+            var secondToLastPoint = foundPoints.ElementAt(foundPoints.Count() - 2);
+
+            Debug.WriteLine($"Second to last point: {secondToLastPoint.Context.Series.Name}");
+        }
+
+        else
+        {
+            Debug.WriteLine("Not enough points to get the second to last element.");
+        }
+    }
+
+
+    public IEnumerable<ISeries> Series { get; set; } =
+        GaugeGenerator.BuildSolidGauge(
+            new GaugeItem(100, series => SetStyle("Вопросов в теме", series, SKColors.Blue)),
+            new GaugeItem(30, series => SetStyle("Ошибок", series, SKColors.Red)),
+            new GaugeItem(70, series => SetStyle("Верных", series, SKColors.Green)),
+            new GaugeItem(10, series => SetStyle("Медленных", series, SKColors.Yellow)),            
+            new GaugeItem(GaugeItem.Background, series =>
+            {
+                series.InnerRadius = 10;
+            }));
+
+    public static void SetStyle(string name, PieSeries<ObservableValue> series, SKColor color)
+    {
+        series.Name = name;
+        series.DataLabelsPosition = PolarLabelsPosition.Start;
+        series.DataLabelsFormatter = point => $"{point.Coordinate.PrimaryValue} {point.Context.Series.Name}";
+        series.InnerRadius = 5;
+        series.RelativeOuterRadius = 2;
+        series.RelativeInnerRadius = 2;
+        series.Fill = new SolidColorPaint(color);
+    }
+
+    
+
+    public MainPage()
 	{
         Shell.Current.Navigating += OnNavigating;
     }
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,51 +1,40 @@
-﻿using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Extensions;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.Input;
+using LiveChartsCore;
 using LiveChartsCore.Defaults;
-using LiveChartsCore.Measure;
+using LiveChartsCore.Drawing;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.Kernel.Events;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace TestMulti
 
 {
     public partial class MainPage : ContentPage
-    {
-
-        public IEnumerable<ISeries> Series { get; set; } =
-        GaugeGenerator.BuildSolidGauge(
-            new GaugeItem(30, series => SetStyle("Vanessa", series)),
-            new GaugeItem(50, series => SetStyle("Charles", series)),
-            new GaugeItem(70, series => SetStyle("Ana", series)),
-            new GaugeItem(GaugeItem.Background, series =>
-            {
-                series.InnerRadius = 20;
-            }));
-
-        public static void SetStyle(string name, PieSeries<ObservableValue> series)
-        {
-            series.Name = name;
-            series.DataLabelsPosition = PolarLabelsPosition.Start;
-            series.DataLabelsFormatter =
-                    point => $"{point.Coordinate.PrimaryValue} {point.Context.Series.Name}";
-            series.InnerRadius = 20;
-            series.RelativeOuterRadius = 8;
-            series.RelativeInnerRadius = 8;
-        }
-
-
-
-
-
+    {    
         public MainPage()
         {
             
             InitializeComponent();
-            //BindingContext = new ViewModels.MainPage();
-            BindingContext = this;
+            BindingContext = new ViewModels.MainPage();
+            
         }
+
+       
+
 
         private void clearbtn_Clicked(object sender, EventArgs e)
         {
             Preferences.Clear();
+
+        }
+
+        private void PieChart_DataPointerDown(IChartView chart, IEnumerable<ChartPoint> points)
+        {
 
         }
     }
